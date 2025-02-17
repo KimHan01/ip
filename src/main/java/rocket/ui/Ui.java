ae1c8591e11a1a34cb38ad5086cce996631c9bca
KimHan01 <teokimhan10@hotmail.com>
2025-02-17 14:16:58 +0800

    Ui.java: slight reformat

--- src/main/java/rocket/ui/Ui.java
+++ src/main/java/rocket/ui/Ui.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  * User Interface class which handles the interaction between user and {@link Rocket}.
- * Contain methods to read responses back to the user.
+ * Contains methods to read input from user and display messages to user.
  */
 public class Ui {
     private Scanner sc;
@@ -38,35 +38,21 @@ public class Ui {
         System.out.println(LINE);
     }
 
+    /**
+     * Reads rocket's introduction message
+     */
+    public void introduction() {
+        read("Oh, great. Another day, another gig as a chatbot. "
+                + "Alright listen up, kid.\nName's Rocket. Rocket Raccoon. Yeah, the genius, weapons expert, "
+                + "and the best pilot you'll ever meet.\nNow, I'm stuck here tracking your tasks instead of "
+                + "blasting through the galaxy with Groot.\nSo, what do ya want? "
+                + "Make it quick, I've got virtual raccoon stuff to do.");
+    }
+
+    /**
+     * Reads file not found message
+     */
     public void fileNotFound() {
         read("File not found\n");
     }
-
-    public void introduction() {
-        read(LINE + "Oh, great. Another day, another gig as a chatbot. " +
-                "Alright listen up, kid.\nName's Rocket. Rocket Raccoon. Yeah, the genius, weapons expert, " +
-                "and the best pilot you'll ever meet.\nNow, I'm stuck here tracking your tasks instead of " +
-                "blasting through the galaxy with Groot.\nSo, what do ya want? " +
-                "Make it quick, I've got virtual raccoon stuff to do.\n" + LINE);
-    }
-
-    /**
-     * Response to trying to mark a task that does not exist.
-     */
-    public void readInvalidMarkRequest() {
-        read(LINE + "Oh, you're trying to mark off a task that doesn't even exist? \n" +
-                "If it's not on the list, it can't be marked as done. \n" +
-                "Check again, maybe the task is hiding in a parallel dimension or something.\n" + LINE);
-    }
-
-    /**
-     * Response to trying to unmark a task that does not exist.
-     */
-    public void readInvalidUnmarkRequest() {
-        read(LINE + "You're trying to unmark a task that doesn't even exist? \n" +
-                "That's some next-level overthinking right there. \n" +
-                "Look, if it's not on the list, there's nothing to unmark. \n" +
-                "Double-check your list, alright?\n" + LINE);
-    }
-
 }
